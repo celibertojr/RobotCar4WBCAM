eeb32f51f2e140575571067ac30256c895a0b9d2
Luiz Antonio Celiberto Junior <celibertojr@gmail.com>
2017-01-24 11:38:19 -0200

    First

---
+++ fonte/display.py
@@ -0,0 +1,228 @@
+#!/usr/bin/python
+
+import smbus
+import pygame
+import urllib
+import time
+import math
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from math import radians
+from pygame.locals import *
+
+# Power management registers
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+
+SCREEN_SIZE = (800, 600)
+SCALAR = .5
+SCALAR2 = 0.2
+
+def read_byte(adr):
+    return bus.read_byte_data(address, adr)
+
+def read_word(adr):
+    high = bus.read_byte_data(address, adr)
+    low = bus.read_byte_data(address, adr+1)
+    val = (high << 8) + low
+    return val
+
+def read_word_2c(adr):
+    val = read_word(adr)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+
+def get_z_rotation(x,y,z):
+    radians = math.atan2(y,z)
+    #radians = math.atan((math.sqrt(x*x+y*y)/z))
+    return math.degrees(radians)
+
+bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
+address = 0x68       # This is the address value read via the i2cdetect command
+
+# Now wake the 6050 up as it starts in sleep mode
+bus.write_byte_data(address, power_mgmt_1, 0)
+
+def get_current_perf(axis):
+	accel_xout = read_word_2c(0x3b)
+	accel_yout = read_word_2c(0x3d)
+	accel_zout = read_word_2c(0x3f)
+	accel_xout_scaled = accel_xout / 16384.0
+	accel_yout_scaled = accel_yout / 16384.0
+	accel_zout_scaled = accel_zout / 16384.0
+
+	if axis == "x":
+		value = round(get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled), 2)
+		print "x rotation:  " , value
+		return value
+	elif axis == "y":
+		value = round(get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled), 2)
+		print "y rotation: " , value
+		return value
+	elif axis == "z":
+		value = round(get_z_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled), 2)
+		print "z rotation: " , value
+		return value
+	else: 
+		return False
+
+def resize(width, height):
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45.0, float(width) / height, 0.001, 10.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    
+def init():
+    glEnable(GL_DEPTH_TEST)
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glShadeModel(GL_SMOOTH)
+    glEnable(GL_BLEND)
+    glEnable(GL_POLYGON_SMOOTH)
+    glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST)
+    glEnable(GL_COLOR_MATERIAL)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_LIGHT0)
+    glLightfv(GL_LIGHT0, GL_AMBIENT, (0.3, 0.3, 0.3, 1.0));
+
+# def read_values():
+#     #link = "http://192.168.1.65:8080" # Change this address to your settings
+#     #f = urllib.urlopen(link)
+#     myfile = "45 45"
+#     return myfile.split(" ")
+
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_SIZE, HWSURFACE | OPENGL | DOUBLEBUF)
+    resize(*SCREEN_SIZE)
+    init()
+    clock = pygame.time.Clock()
+    cube = Cube((0.0, 0.0, 0.0), (.5, .5, .7))
+    glTranslatef(0.0,0.0, -5)
+    while True:
+        then = pygame.time.get_ticks()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+            if event.type == KEYUP and event.key == K_ESCAPE:
+                return
+
+        y_angle = get_current_perf('y')
+        #z_angle = get_current_perf('z')
+        x_angle = get_current_perf('x')
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        glColor((1.,1.,1.))
+        glLineWidth(1)
+        glBegin(GL_LINES)
+
+        for x in range(-20, 22, 2):
+            glVertex3f(x/10.,-1,-1)
+            glVertex3f(x/10.,-1,1)
+        
+        for x in range(-20, 22, 2):
+            glVertex3f(x/10.,-1, -1)
+            glVertex3f(x/10., 1, -1)
+        
+        for z in range(-10, 12, 2):
+            glVertex3f(-2, -1, z/10.)
+            glVertex3f( 2, -1, z/10.)
+
+        for z in range(-10, 12, 2):
+            glVertex3f(-2, -1, z/10.)
+            glVertex3f(-2,  1, z/10.)
+
+        for z in range(-10, 12, 2):
+            glVertex3f( 2, -1, z/10.)
+            glVertex3f( 2,  1, z/10.)
+
+        for y in range(-10, 12, 2):
+            glVertex3f(-2, y/10., -1)
+            glVertex3f( 2, y/10., -1)
+        
+        for y in range(-10, 12, 2):
+            glVertex3f(-2, y/10., 1)
+            glVertex3f(-2, y/10., -1)
+        
+        for y in range(-10, 12, 2):
+            glVertex3f(2, y/10., 1)
+            glVertex3f(2, y/10., -1)
+        
+        glEnd()
+        glPushMatrix()
+        glRotate(float(y_angle), 1, 0, 0)
+        glRotate(float(x_angle), 0, 0, 1)
+        #glRotate(float(z_angle), 0, 1, 0)
+        cube.render()
+        glPopMatrix()
+        pygame.display.flip()
+        time.sleep(0.1)
+
+class Cube(object):
+
+    def __init__(self, position, color):
+        self.position = position
+        self.color = color
+
+    # Cube information
+    num_faces = 6
+
+    vertices = [ (-1.0, -0.05, 0.5),
+                 (1.0, -0.05, 0.5),
+                 (1.0, 0.05, 0.5),
+                 (-1.0, 0.05, 0.5),
+                 (-1.0, -0.05, -0.5),
+                 (1.0, -0.05, -0.5),
+                 (1.0, 0.05, -0.5),
+                 (-1.0, 0.05, -0.5) ]
+
+    normals = [ (0.0, 0.0, +1.0),  # front
+                (0.0, 0.0, -1.0),  # back
+                (+1.0, 0.0, 0.0),  # right
+                (-1.0, 0.0, 0.0),  # left
+                (0.0, +1.0, 0.0),  # top
+                (0.0, -1.0, 0.0) ]  # bottom
+
+    vertex_indices = [ (0, 1, 2, 3),  # front
+                       (4, 5, 6, 7),  # back
+                       (1, 5, 6, 2),  # right
+                       (0, 4, 7, 3),  # left
+                       (3, 2, 6, 7),  # top
+                       (0, 1, 5, 4) ]  # bottom
+
+    def render(self):
+        then = pygame.time.get_ticks()
+        glColor(self.color)
+
+        vertices = self.vertices
+
+        # Draw all 6 faces of the cube
+        glBegin(GL_QUADS)
+
+        for face_no in xrange(self.num_faces):
+            glNormal3dv(self.normals[face_no])
+            v1, v2, v3, v4 = self.vertex_indices[face_no]
+            glVertex(vertices[v1])
+            glVertex(vertices[v2])
+            glVertex(vertices[v3])
+            glVertex(vertices[v4])
+        glEnd()
+
+if __name__ == "__main__":
+    run()
+    time.sleep(1)
